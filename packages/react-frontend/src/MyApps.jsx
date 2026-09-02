9771c9bff86a5a043d055bc380e0f292953a559d
Sean O <82001107+seanoyama@users.noreply.github.com>
2026-09-01 21:51:25 -0700

    finished IE1 contents

---
+++ packages/react-frontend/src/MyApps.jsx
@@ -0,0 +1,12 @@
+// src/MyApp.jsx
+import React from "react";
+import Table from "./Table";
+function MyApp() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+	  <Table />
+    </div>
+  );
+}
+export default MyApp;
