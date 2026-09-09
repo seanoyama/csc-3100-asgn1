655eb8d2bae60c3255248851c0bf3cf344b80bb5
Sean O <82001107+seanoyama@users.noreply.github.com>
2026-09-08 19:50:22 -0700

    IE2 step 4

--- node_modules/express-backend/backend.js
+++ node_modules/express-backend/backend.js
@@ -4,16 +4,6 @@ import express from "express";
 const app = express();
 const port = 8000;
 
-app.use(express.json());
-
-app.get("/users", (req, res) => {
-  res.send(users);
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-
 const users = {
   users_list: [
     {
@@ -43,3 +33,30 @@ const users = {
     },
   ],
 };
+
+const findUserByName = (name) => {
+  return users["users_list"].filter((user) => user["name"] === name);
+};
+
+app.use(express.json());
+
+app.get("/",(req,res) => {
+  res.send("Hello World!");
+});
+
+app.get("/users", (req, res) => {
+  // res.send(users);
+  const name = req.query.name;
+  if (name != undefined){
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  }
+  else{
+    res.send(users);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
